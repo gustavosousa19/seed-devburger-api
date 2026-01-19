4fafa140b7406d0fa73582ab5ece8beee339478d
Gustavo <beckhamgustavo0@gmail.com>
2026-01-19 13:57:10 -0300

    controller login de usuário

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,5 +1,58 @@
-class SessionController {
+import bcrypt from 'bcrypt';
+import * as Yup from 'yup';
+import User from '../models/User.js';
 
+// VERIFICA SE OS DADOS DE LOGIN SÃO VÁLIDOS
+class SessionController {
+ async store(request, response) {
+    const schema = Yup.object({
+        email: Yup.string().email().required(),
+        password: Yup.string().min(6).required(),
+    });
+
+    const isValid = await schema.isValid(request.body, { 
+      abortEarly: false, 
+      strict: true, 
+    });
+
+    const emailOrPasswordIncorrect = () => {
+        return response
+        .status(400)
+        .json({ error: 'Email or password incorrect' });
+    }
+    
+    if(!isValid){
+        emailOrPasswordIncorrect();
+    };
+  
+    const { email, password } = request.body;
+
+    // VERIFICA SE O EMAIL EXISTE
+    const existingUser = await User.findOne({ 
+      where: {
+        email, 
+      },
+    });
+
+    if (!existingUser) {
+      emailOrPasswordIncorrect();          
+    };
+
+    // VERIFICA SE A SENHA ESTÁ CORRETA
+    const isPasswordCorrect = await bcrypt.compare(password, existingUser.password_hash,);
+
+    if (!isPasswordCorrect) {
+      emailOrPasswordIncorrect();           
+    };
+
+    // RETORNA OS DADOS DO USUÁRIO
+    return response.status(200).json({ 
+        id: existingUser.id,
+        name: existingUser.name,
+        email: existingUser.email,
+        admin: existingUser.admin,
+     });
+ }
 }
 
 export default new SessionController();
