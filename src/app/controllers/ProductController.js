7898c979bfaaf222483c46ae23334e9308972f4f
Gustavo <beckhamgustavo0@gmail.com>
2026-02-16 14:48:23 -0300

    ajuste no controller de produtos

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -1,12 +1,13 @@
 import * as Yup from 'yup';
 import Product from '../models/Product.js';
+import Category from '../models/Category.js';
 
 class ProductController {
     async store(request, response) {
         const schema = Yup.object({
             name: Yup.string().required(),
             price: Yup.number().required(),
-            category: Yup.string().required(),
+            category_id: Yup.number().required(),
         });
 
         try {
@@ -15,13 +16,13 @@ class ProductController {
           return response.status(400).json({ error: err.errors });
         }
         
-        const { name, price, category } = request.body; // DESESTRUTURAÇÃO DOS DADOS RECEBIDOS NA REQUISIÇÃO
+        const { name, price, category_id } = request.body; // DESESTRUTURAÇÃO DOS DADOS RECEBIDOS NA REQUISIÇÃO
         const { filename } = request.file; // PEGANDO O NOME DO ARQUIVO ENVIADO
         
         const newProduct = await Product.create({ // CRIA UM NOVO PRODUTO NO BANCO DE DADOS
           name,
           price,
-          category,
+          category_id,
           path: filename,
         });
 
@@ -29,7 +30,13 @@ class ProductController {
   }
 
   async index(_request, response) {
-    const products = await Product.findAll(); // BUSCA TODOS OS PRODUTOS NO BANCO DE DADOS
+    const products = await Product.findAll({ // BUSCA TODOS OS PRODUTOS NO BANCO DE DADOS
+      include: { // INCLUI A CATEGORIA ASSOCIADA A CADA PRODUTO
+        model: Category, // QUAL MODEL QUE DEVE SER INCLUÍDA
+        as: 'category', // QUAL O NOMDE DA PROPRIEDADE, TEM QUE ESTA DE ACORDO COM O NOME DEFINIDO NA MODELS
+        attributes: ['id', 'name'], // SELECIONA APENAS OS CAMPOS ID E NAME DA CATEGORIA PARA SEREM RETORNADOS NA RESPOSTA
+      },
+    }); 
     
     return response.status(200).json(products);
   }
