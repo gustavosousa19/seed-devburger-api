e1f8a15d7f3253d7b867b631b75a3a9194483104
Gustavo <beckhamgustavo0@gmail.com>
2026-03-10 14:51:11 -0300

    conectando com o front-end

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 // AONDE DECLARA A VARIAVEL PRA UTILIZAR DE TODOAS AS FUNCIONALIDADES DO EXPRESS
 import express from 'express';
 import fileRouteConfig from './config/fileRoutes.cjs';
-import routes from './routes.js'; // IMPORTA AS ROTAS DO ARQUIVO ROUTES.JS
+import cors from 'cors';
 
+import routes from './routes.js'; // IMPORTA AS ROTAS DO ARQUIVO ROUTES.JS
 
 const app = express(); // DECLARA A VARIAVEL APP COMO UMA INSTANCIA DO EXPRESS
 
 app.use(express.json()); // PERMITE QUE O EXPRESS ENTENDA REQUISIÇÕES COM JSON
+app.use(cors()); // HABILITA O CORS PARA PERMITIR REQUISIÇÕES DE ORIGENS DIFERENTES
 app.use(express.urlencoded({ extended: true })); // PADRÃO DE URL PRA UTILIZAR O BODY
 app.use('/product-file', fileRouteConfig); // ROTA PARA SERVIR OS ARQUIVOS ESTÁTICOS DE PRODUTOS
 app.use('/category-file', fileRouteConfig);
